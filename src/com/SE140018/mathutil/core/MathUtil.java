7ffb05926819e52a186eb997e5770b3c1f69a5af
hungnln <nguyenhung09082000@yahoo.com>
2021-09-30 08:27:22 +0700

    use recursion to demostrate Regression Testing and GREEN

--- src/com/SE140018/mathutil/core/MathUtil.java
+++ src/com/SE140018/mathutil/core/MathUtil.java
@@ -13,19 +13,29 @@ public class MathUtil {
 
     /**
      * @param n
-     * @return 
+     * @return
      */
-    public static long getFactorial(int  n){
-        if(n <0 || n>20){
-            throw new IllegalArgumentException("n must be between 0..20");
+//    public static long getFactorial(int  n){
+//        if(n <0 || n>20){
+//            throw new IllegalArgumentException("n must be between 0..20");
+//        }
+//        if(n==0||n==1){
+//            return 1;
+//        }
+//        long product =1;
+//        for (int i = 1; i <= n; i++) {
+//            product *= i;
+//        }
+//    return product;
+    //De quy, recursion, goi lai chinh minh voi 1 quy mo khac
+    //n!=n*(n-1)!
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("Invalid n between 1 and 20");
         }
-        if(n==0||n==1){
+        if (n == 0 || n == 1) {
             return 1;
         }
-        long product =1;
-        for (int i = 1; i <= n; i++) {
-            product *= i;
-        }
-    return product;
+        return n * getFactorial(n - 1);
     }
 }
